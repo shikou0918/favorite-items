9e9a442c3a576930861062d80306f95e0e443ade
@shikou0918 <shikou.0918@gmail.com>
2020-04-25 17:04:21 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   devise_for :users
   devise_scope :user do
     post 'users/guest_sign_in' => 'users/sessions#new_guest'
   end
   root "home#top"
+  resources :users, only: %i[edit show] do
+  end
 end
