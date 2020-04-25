9fd102b352eb85af19748bda8be88675adabedbc
@shikou0918 <shikou.0918@gmail.com>
2020-04-25 17:03:30 +0900

    マイグレーションファイル修正

--- db/migrate/20200424124704_create_posts.rb
+++ db/migrate/20200424124704_create_posts.rb
@@ -1,11 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
+      t.timestamps
       t.references :user, null: false, foreign_key: true
       t.string :name, null: false
       t.text :content, null: false
       t.text :image, null: false
-      t.timestamps
     end
   end
 end
