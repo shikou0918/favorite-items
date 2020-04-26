360afdddd41e6244a71868b4c66f8d4157a02af8
@shikou0918 <shikou.0918@gmail.com>
2020-04-26 16:18:17 +0900

    Update search

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts do
+     collection do
+      get 'search'
+    end
     resources :comments
   end
   devise_for :users
