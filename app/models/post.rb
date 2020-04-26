6b03a5b5318c92e76f841efa8085865bea00efe5
@shikou0918 <shikou.0918@gmail.com>
2020-04-26 16:30:29 +0900

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,12 @@ class Post < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 40 }
   validates :content,       presence: true, length: { maximum: 300 }
   validates :image,         presence: true
+
+  def self.search(search)
+    if search
+      Post.where(['name LIKE ?', "%#{search}%"])
+    else
+      Post.all
+    end
+  end
 end
