c2f6cd71f880124f9c40e70e8aa74efe8d2ab5a0
@shikou0918 <shikou.0918@gmail.com>
2020-04-26 20:56:56 +0900

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user, optional: true
   has_many :comments
+  has_many :likes, dependent: :destroy
   mount_uploader :image, ImageUploader
   validates :name,  presence: true, length: { maximum: 40 }
   validates :content,       presence: true, length: { maximum: 300 }
   validates :image,         presence: true
+
 
   def self.search(search)
     if search
@@ -13,4 +15,7 @@ class Post < ApplicationRecord
       Post.all
     end
   end
+  def like_user(user_id)
+    likes.find_by(user_id: user_id)
+   end
 end
