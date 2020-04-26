607b5382501bfbcf6ddb7721dd024a04e33a4dd8
@shikou0918 <shikou.0918@gmail.com>
2020-04-26 20:56:37 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'likes/create'
+
+  get 'likes/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts do
      collection do
       get 'search'
     end
     resources :comments
+    resources :likes, only: [:create, :destroy]
   end
   devise_for :users
   devise_scope :user do
