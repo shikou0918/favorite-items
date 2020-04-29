9d284840f674c6b0202b61e255bd9dfe8f670258
@shikou0918 <shikou.0918@gmail.com>
2020-04-29 16:51:32 +0900

    user_spec.rbの生成

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+describe User do
+  describe '#create' do
+    it "名前、メールアドレス、パスワードがあれば登録できる" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+   
+    it "名前がないと登録できない" do
+      user = build(:user, nickname: "")
+      user.valid?
+      expect(user.errors[:nickname]).to include("can't be blank")
+    end
+
+    it "名前が11文字以上だと登録できない" do
+      user = build(:user, nickname: "aaaaaaaaaaa")
+      user.valid?
+      expect(user.errors[:nickname]).to include("is too long (maximum is 10 characters)")
+    end
+
+
+    it "メールアドレスがない場合は登録できない" do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it "パスワードがない場合は登録できない" do
+    user = build(:user, password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it "パスワードが6文字以内だと登録できない" do
+      user = build(:user, password: "00000")
+      user.valid?
+      expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
+    end
+
+    it "パスワードと確認が一致していないと登録できない" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+
+    it "重複したアドレスは登録できない" do
+      user = create(:user)
+      another_user = build(:user)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
+  end
+end
