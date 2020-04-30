c00e85a9a446fd1ffdac96c28b07f2ca0e5ff9e6
@shikou0918 <shikou.0918@gmail.com>
2020-04-30 18:40:34 +0900

    post_idとuser_idの組が1組しかないようにした

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,5 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post, counter_cache: :likes_count
-  validates :user_id, presence: true
-  validates :post_id, presence: true
+  validates_uniqueness_of :post_id, scope: :user_id
 end
