c5e5cd6befd4c8d08fa0cc10f6597582e868c936
@shikou0918 <shikou.0918@gmail.com>
2020-04-30 12:13:42 +0900

    postsテーブルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -32,13 +32,13 @@ ActiveRecord::Schema.define(version: 20200426081253) do
   end
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.integer  "user_id",                   null: false
-    t.string   "name",                      null: false
-    t.text     "content",     limit: 65535, null: false
-    t.string   "image",                     null: false
-    t.integer  "likes_count"
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+    t.integer  "user_id",                               null: false
+    t.string   "name",                                  null: false
+    t.text     "content",     limit: 65535,             null: false
+    t.string   "image",                                 null: false
+    t.integer  "likes_count",               default: 0
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
