259314848b439f7804154d0bdf94291f092eb029
@shikou0918 <shikou.0918@gmail.com>
2020-04-30 14:12:08 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,14 @@
 class UsersController < ApplicationController
-  # before_action :authenticate_user!, only: [:show]
-  before_action :set_user
+  # before_action :set_user
 
   def index; end
 
   def show
-    @user = User.find_by(id: params[:id])
+    @user = User.find(params[:id])
     @posts = @user.posts.page(params[:page]).order('created_at DESC')
   end
 
-  def set_user
-    @user = User.find(params[:id])
-  end
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
 end
