9adb396f98766aaba6c3fa86569c3ebe0e6ee57c
@shikou0918 <shikou.0918@gmail.com>
2020-05-02 20:19:03 +0900

    FactoryBot編集

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,5 +3,8 @@ FactoryBot.define do
     name {"テスト"}
     content {"紹介文"}
     image {Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg'))}
+    likes_count { "1" }
+    created_at { Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
+    user
   end
 end
