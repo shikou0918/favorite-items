e17b5fa3479b09aa4329207b680a319faa9f3b30
@shikou0918 <shikou.0918@gmail.com>
2020-05-02 13:12:25 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,16 @@ class UsersController < ApplicationController
     @liked_posts = @user.liked_posts.page(params[:page]).order('created_at DESC').page(params[:page]).per(9)
   end
 
+  def following
+    @user  = User.find(params[:id])
+    @users = @user.followings
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.followers
+  end
+
   # def set_user
   #   @user = User.find(params[:id])
   # end
