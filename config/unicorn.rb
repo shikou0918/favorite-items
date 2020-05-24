476aa6f4be84802135cb928256057ea3401ab217
@shikou0918 <shikou.0918@gmail.com>
2020-05-25 05:57:08 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,57 +1,55 @@
-# Railsのルートパスを求める。(RAILS_ROOT/config/unicorn.rbに配置している場合。)
-rails_root = File.expand_path('../../', __FILE__)
-# RAILS_ENVを求める。（RAILS_ENV毎に挙動を変更したい場合に使用。今回は使用しません。)
-# rails_env = ENV['RAILS_ENV'] || "development"
+# サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
+rails_root = File.expand_path('../../../', __FILE__)
 
-# 追記に記載してます。入れた方がいいです。
-ENV['BUNDLE_GEMFILE'] = rails_root + "/Gemfile"
+# アプリケーションサーバの性能を決定する
+worker_processes 1
 
-# Unicornは複数のワーカーで起動するのでワーカー数を定義
-# サーバーのメモリなどによって変更すること。
-worker_processes 2
-
-# 指定しなくても良い。
-# Unicornの起動コマンドを実行するディレクトリを指定します。
-# （記載しておけば他のディレクトリでこのファイルを叩けなくなる。）
+# アプリケーションの設置されているディレクトリを指定
 working_directory rails_root
 
-# 接続タイムアウト時間
-timeout 30
+# Unicornの起動に必要なファイルの設置場所を指定
+pid "#{rails_root}/shared/tmp/pids/unicorn.pid"
 
-# Unicornのエラーログと通常ログの位置を指定。
-stderr_path File.expand_path('../../log/unicorn_stderr.log', __FILE__)
-stdout_path File.expand_path('../../log/unicorn_stdout.log', __FILE__)
+# ポート番号を指定
+listen "#{rails_root}/shared/tmp/sockets/unicorn.sock"
 
-# Nginxで使用する場合は以下の設定を行う。
-# listen File.expand_path('../../tmp/sockets/unicorn.sock', __FILE__)
-# とりあえず起動して動作確認をしたい場合は以下の設定を行う。
-listen 8080
-# ※「backlog」や「tcp_nopush」の設定もあるけど、よくわかって無い。
+# エラーのログを記録するファイルを指定
+stderr_path "#{rails_root}/shared/log/unicorn.stderr.log"
 
-# プロセスの停止などに必要なPIDファイルの保存先を指定。
-pid File.expand_path('../../tmp/pids/unicorn.pid', __FILE__)
+# 通常のログを記録するファイルを指定
+stdout_path "#{rails_root}/shared/log/unicorn.stdout.log"
 
-# 基本的には`true`を指定する。Unicornの再起動時にダウンタイムなしで再起動が行われる。
+# Railsアプリケーションの応答を待つ上限時間を設定
+timeout 60
+
+# 以下は応用的な設定なので説明は割愛
+
 preload_app true
-# 効果なしとの記事を見たので、コメントアウト。
-# GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
+GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
 
-# USR2シグナルを受けると古いプロセスを止める。
-# 後述するが、記述しておくとNginxと連携する時に良いことがある。
+check_client_connection false
+
+run_once = true
+
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and
-      ActiveRecord::Base.connection.disconnect!
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
+
+  if run_once
+    run_once = false # prevent from firing again
+  end
 
   old_pid = "#{server.config[:pid]}.oldbin"
-  if old_pid != server.pid
+  if File.exist?(old_pid) && server.pid != old_pid
     begin
       sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
       Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
+    rescue Errno::ENOENT, Errno::ESRCH => e
+      logger.error e
     end
   end
 end
 
-after_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+after_fork do |_server, _worker|
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
