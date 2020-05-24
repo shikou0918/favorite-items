3bfd5776dca489afc5c5f3564979d536f139b734
@shikou0918 <shikou.0918@gmail.com>
2020-05-25 06:01:53 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,9 @@
-# サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../../', __FILE__)
-
-# アプリケーションサーバの性能を決定する
-worker_processes 1
-
-# アプリケーションの設置されているディレクトリを指定
-working_directory "#{app_path}/current"
+# 実行したファイルの場所からrailsのrootパスを見つける
+RAILS_ROOT = File.expand_path('../../../', __FILE__)
+ENV['BUNDLE_GEMFILE'] = RAILS_ROOT + "/Gemfile"
+# ここは、RIALS_ROOTが記載されていれば、明示的にかかなくても大丈夫です。
+# 自分が使っている環境でも、下の記載なしにちゃんと動いてますね。
+working_directory RAILS_ROOT
 
 # Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
